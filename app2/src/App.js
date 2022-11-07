8b963a9d10904d5f9a5bdad63ac4ddeb6de35430
shauryaguleria007 <shauryaguleria12345678@gmail.com>
2022-11-07 21:29:52 +0530

    project 2

--- app2/src/App.js
+++ app2/src/App.js
@@ -1,6 +1,10 @@
-
+import { useState } from "react";
+import Loading from "./loading";
+const url = "https://course-api.netlify.app/api/react-tours-project";
 function App() {
-  return <div>hello world</div>;
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  if (loading) return <Loading />;
 }
 
 export default App;
